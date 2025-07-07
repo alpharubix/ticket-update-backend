eb3ccb257007939176e97658e52493f6dc981289
ashok <ashokmommyguy@gmail.com>
2025-07-07 17:38:43 +0530

    added cors

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import uvicorn
 from routes.upload_excel import router
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 app.include_router(router)
+
 
 
